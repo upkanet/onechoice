02189e0f80db37e355d1046e86b759c3cd5e3e0e
Upkanet <upkanet@gmail.com>
2015-01-04 23:04:27 +0100

    Homepage restyling

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,21 +7,22 @@ One Choice - Only the Good Product for each Need
 @section('css')
 	@parent
 	{{ HTML::style('css/index.css') }}
+	{{ HTML::style('css/steveeffect.css') }}
 @stop
 
 @section('content')
 <div class="container">
-	<div class="row">
-	@foreach($products as $product)
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-		    <a href="{{ $product->category->permalink }}/{{ $product->permalink }}" class="thumbnail thbox" style="background: url('{{$product->img_path}}') no-repeat center center;">
-		    	<div class="thprodname">
-		      		<h3>{{ $product->category->name }}</h3>
-		      		<h4>{{ $product->name }}</h4>
-		      	</div>
-		    </a>
-		</div>
-	@endforeach
+	<div class="grid">
+		@foreach($products as $product)
+		<figure class="effect-steve">
+			{{ HTML::image($product->img_path, 'a picture', array('width' => '480')) }}
+			<figcaption>
+				<h2>{{ $product->category->name }}</h2>
+				<p>{{ $product->name }}</p>
+				<a href="{{ $product->category->permalink.'/'.$product->permalink }}">View more</a>
+			</figcaption>			
+		</figure>
+		@endforeach
 	</div>
 </div>
 @stop
