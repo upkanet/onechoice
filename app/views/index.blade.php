e83bd6d4eab5e8a4de73416063919903ee5b6a71
Upkanet <upkanet@gmail.com>
2015-01-04 14:38:31 +0100

    Index prod mode and default product image

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -21,30 +21,6 @@ One Choice - Only the Good Product for each Need
 		      	</div>
 		    </a>
 		</div>
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-		    <a href="{{ $product->category->permalink }}/{{ $product->permalink }}" class="thumbnail thbox" style="background: url('{{$product->img_path}}') no-repeat center center;">
-		    	<div class="thprodname">
-		      		<h3>{{ $product->category->name }}</h3>
-		      		<h4>{{ $product->name }}</h4>
-		      	</div>
-		    </a>
-		</div>
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-		    <a href="{{ $product->category->permalink }}/{{ $product->permalink }}" class="thumbnail thbox" style="background: url('{{$product->img_path}}') no-repeat center center;">
-		    	<div class="thprodname">
-		      		<h3>{{ $product->category->name }}</h3>
-		      		<h4>{{ $product->name }}</h4>
-		      	</div>
-		    </a>
-		</div>
-		<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-		    <a href="{{ $product->category->permalink }}/{{ $product->permalink }}" class="thumbnail thbox" style="background: url('{{$product->img_path}}') no-repeat center center;">
-		    	<div class="thprodname">
-		      		<h3>{{ $product->category->name }}</h3>
-		      		<h4>{{ $product->name }}</h4>
-		      	</div>
-		    </a>
-		</div>
 	@endforeach
 	</div>
 </div>
