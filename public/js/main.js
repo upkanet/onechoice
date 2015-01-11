55af987f6b36b8d80e2e85b9b575b53565ad4851
Upkanet <upkanet@gmail.com>
2015-01-11 12:12:31 +0100

    Search optimized

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,24 @@
 function searchProduct(rootDir){
 	var q = $('#searchInput').val();
-	var r = $('input[name=current_room_id]').val();
+	var room = $('input[name=current_room_id]').val();
+	if('room' == null){room = 0;}
 
-	if('r' == null){
-		r = 0;
-	}
-	if(q!=''){
-		$.get(rootDir+'search/'+r+'/'+q,{},function(data){
-			var target = $('#content');
-			target.html('').append('<div class="container-fluid"><div class="grid" id="searchThumb"></div></div>');
-			data.forEach(function(element,index,array){
-				thumb = '<figure class="effect-steve"><img src="' + element.pImg + '" width="480" alt="' + element.pName + '"><figcaption><h2>' + element.cName + '</h2><p>' + element.pName + '</p><a href="' + element.cLink + '/' + element.pLink + '">View more</a></figcaption></figure>';
-				$('#searchThumb').append(thumb);
-			});
-		},'json');
-	}
-};
+	getSearchResult(rootDir,q,room).success(function(data){
+		var resultDiv = $('#content');
+		var htmlResult = "";
+		data.forEach(function(element,index,array){
+			htmlResult += '<figure class="effect-steve"><img src="' + element.pImg + '" width="480" alt="' + element.pName + '"><figcaption><h2>' + element.cName + '</h2><p>' + element.pName + '</p><a href="' + element.cLink + '/' + element.pLink + '">View more</a></figcaption></figure>';
+		});
+		resultDiv.html('<div class="container-fluid"><div class="grid" id="searchThumb"></div></div>');
+		var searchThumb = $('#searchThumb');
+		searchThumb.append(htmlResult);
+	});
+}
+
+function getSearchResult(rootDir,q,room){
+	return $.ajax({
+		url: rootDir+'search/'+room+'/'+q,
+		type: 'GET',
+		dataType: 'json'
+	});
+}
