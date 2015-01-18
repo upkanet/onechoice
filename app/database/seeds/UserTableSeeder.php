d5e223858a865ab3941c5aedd7eb7a593639c5fc
Upkanet <upkanet@gmail.com>
2015-01-18 16:07:16 +0100

    Admin User

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -7,7 +7,7 @@ class UserTableSeeder extends Seeder {
 		DB::table('users')->delete();
 		User::create(array(
 			'email' => 'jeandamien.louise@gmail.com',
-			'password' => Hash::make('jaydee'),
+			'password' => '$2y$10$XahZlDZ016ey7FVWn2TX9ew9AJhD9ygFkuw0amfTGyH1vsb5xJt4i',
 			'isAdmin' => '1'
 		));
 	}
